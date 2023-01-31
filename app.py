292556a66f9153d82786caa60da469727fd85702
Phillip <phillipmiller@sandiego.edu>
2023-01-31 10:59:11 -0800

    Added comments

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 app = Flask(__name__)
 
+#TODO E Wrtie Cache Object Array and API
+
+#TODO P Write friends API and Data Structure
 @app.route("/")
 def hello_world():
     return "<p>hello world</p>"
