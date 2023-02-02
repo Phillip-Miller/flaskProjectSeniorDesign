76d791c2194aa020f84884ff8ff719c215aedd9a
Phillip <phillipmiller@sandiego.edu>
2023-02-01 22:24:44 -0800

    Fix test.py to use json part of request and not the key response code

--- app.py
+++ app.py
@@ -1,49 +1,27 @@
 from flask import Flask
 from flask_restful import Api, Resource
 
+# eventually want to mock and insert dummy data instead of storing here
+friendDataStructure = {"user0": [["Immaculata"], ["user1"]], "user1": [["Saints"], ["user0", "user4"]],
+                       "user2": [["UC's"], ["user4"]], "user3": [["BEC"], []], "user4": [["SLP"], ["user1", "user2"]]}
+
 app = Flask(__name__)
 api = Api(app)
 
 
 # TODO E Write Cache Object Array and API
 
-def create_fake_user_data() -> dict:
-    '''
-    Create ssample fake user data using data structure defined in deliverables
-
-    Usernames are user1,user2....
-    User data structure {User : [[Caches], [Friends]]}
-    completedCacheList = ["Immaculata", "Saints", "UC's", "BEC", "SLP"]
-    friendsRelations = {1:[2], 2:[1,2], 3:[5], 4:[], 5:[3,2]}
-
-    Useri has completed element i in completedCacheList and dict is
-    indexed by user
-    '''
-    returnDict = {}
-
-    completedCacheList = ["Immaculata", "Saints", "UC's", "BEC", "SLP"]
-    friendsRelations = {0: ["user1"], 1: ["user0", "user4"], 2: ["user4"], 3: [], 4: ["user1", "user2"]}
-
-    for userNumber in range(5):
-        user_name = "user" + str(userNumber)
-        returnDict[user_name] = [[completedCacheList[userNumber]], friendsRelations[userNumber]]
-    return returnDict
-
-
-friendDataStructure = create_fake_user_data()
-
-
 class UserData(Resource):
     def get(self):
         return friendDataStructure
 
 
-api.add_resource(UserData, "/UserData")  # configure url to acsess resource
+api.add_resource(UserData, "/UserData")
 
 
 @app.route("/")
-def hello_world():
-    return "<p>hello world</p>"
+def UniversityGeocache():
+    return "<p>University Geocaching Homepage </p>"
 
 
 # change for production env
--- test.py
+++ test.py
@@ -2,5 +2,11 @@ import requests
 import pytest
 
 BASE = "http://127.0.0.1:5000/"
-response = requests.get(BASE + "UserData")
-print(response);
+
+
+def test_get_user_data():
+    expected = friendDataStructure = {"user0": [["Immaculata"], ["user1"]], "user1": [["Saints"], ["user0", "user4"]],
+                                      "user2": [["UC's"], ["user4"]], "user3": [["BEC"], []],
+                                      "user4": [["SLP"], ["user1", "user2"]]}
+    response = requests.get(BASE + "UserData")
+    assert (response.json() == expected)
