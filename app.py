3eefd0fc33dd6fac87590cef14cbedd6a96c7eb1
Ewhondens Kenel <ekenel@sandiego.edu>
2023-02-06 15:45:11 -0800

    added what i have before we meet

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Api, Resource
 
 # eventually want to mock and insert dummy data instead of storing here
+list_of_locations = []
+
 friendDataStructure = {"user0": [["Immaculata"], ["user1"]], "user1": [["Saints"], ["user0", "user4"]],
                        "user2": [["UC's"], ["user4"]], "user3": [["BEC"], []], "user4": [["SLP"], ["user1", "user2"]]}
 
@@ -10,6 +12,62 @@ api = Api(app)
 
 
 # TODO E Write Cache Object Array and API
+
+class Location(object):
+
+    # Constuctor
+    def __init__(self,name):
+        self.name = name
+
+    # getter / setter
+    def get_name(self):
+        return self.name
+
+    def set_age(self, newName):
+        if newName < 0:
+            return
+        self.name = newName
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Locations(object):
+
+    def __init__(self, locations):
+        self.locations = locations
+
+    def delete(self, item):
+        self.locations.remove(item)
+
+    def __str__(self):
+        x = []
+        for Location in self.locations:
+            x.append(str(Location))
+
+        return str(x)
+
+gym = Location("gym")
+lib = Location("lib")
+lab = Location("lab")
+
+list_of_locations = Locations([gym, lib, lab])
+
+
+print(list_of_locations)
+
+list_of_locations.delete(gym)
+
+print(list_of_locations)
+
+
+api.add_resource(CACHE_OBJECT_LOCATION, "/CACHEOBJECT")
+
+
+@app.route("/")
+def UniversityGeocache():
+    return "<p>University Geocaching Homepage </p>"
+
 
 class UserData(Resource):
     def get(self):
