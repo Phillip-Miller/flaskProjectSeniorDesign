762f2df1dab143ddbe65a75216950e8bfe288cf4
Ewhondens Kenel <ekenel@sandiego.edu>
2023-02-06 16:10:36 -0800

    Added my code, cleaned up

--- app.py
+++ app.py
@@ -10,41 +10,30 @@ friendDataStructure = {"user0": [["Immaculata"], ["user1"]], "user1": [["Saints"
 app = Flask(__name__)
 api = Api(app)
 
-
 # TODO E Write Cache Object Array and API
 
 class Location(object):
-
-    # Constuctor
     def __init__(self,name):
         self.name = name
-
     # getter / setter
     def get_name(self):
         return self.name
-
-    def set_age(self, newName):
+    def set_Name(self, newName):
         if newName < 0:
             return
         self.name = newName
-
     def __str__(self):
         return str(self.name)
 
-
 class Locations(object):
-
     def __init__(self, locations):
         self.locations = locations
-
     def delete(self, item):
         self.locations.remove(item)
-
     def __str__(self):
         x = []
         for Location in self.locations:
             x.append(str(Location))
-
         return str(x)
 
 gym = Location("gym")
@@ -53,28 +42,17 @@ lab = Location("lab")
 
 list_of_locations = Locations([gym, lib, lab])
 
-
+#testing below
 print(list_of_locations)
-
 list_of_locations.delete(gym)
-
 print(list_of_locations)
-
-
-api.add_resource(CACHE_OBJECT_LOCATION, "/CACHEOBJECT")
-
-
-@app.route("/")
-def UniversityGeocache():
-    return "<p>University Geocaching Homepage </p>"
-
 
 class UserData(Resource):
     def get(self):
         return friendDataStructure
 
-
 api.add_resource(UserData, "/UserData")
+api.add_resource(Location, "/CacheObjectLocations")
 
 
 @app.route("/")
