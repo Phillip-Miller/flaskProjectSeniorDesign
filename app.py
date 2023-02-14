d72c29d29bbeeb72f995addb402e4a767ce8dfab
Ewhondens Kenel <ekenel@sandiego.edu>
2023-02-14 14:57:48 -0800

    added get and post methods

--- app.py
+++ app.py
@@ -1,59 +1,49 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 # eventually want to mock and insert dummy data instead of storing here
-list_of_locations = []
+
+list_of_locations = {1:"Immaculata"}
+
 
 friendDataStructure = {"user0": [["Immaculata"], ["user1"]], "user1": [["Saints"], ["user0", "user4"]],
                        "user2": [["UC's"], ["user4"]], "user3": [["BEC"], []], "user4": [["SLP"], ["user1", "user2"]]}
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
 
-# TODO E Write Cache Object Array and API
+class location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    locationName = db.Column(db.String(80), unique=True, nullable=False)
 
-class Location(object):
-    def __init__(self,name):
-        self.name = name
-    # getter / setter
-    def get_name(self):
-        return self.name
-    def set_Name(self, newName):
-        if newName < 0:
-            return
-        self.name = newName
-    def __str__(self):
-        return str(self.name)
+    def __repr__(self):
+        return '<location %r>' % self.locationName
 
-class Locations(object):
-    def __init__(self, locations):
-        self.locations = locations
-    def delete(self, item):
-        self.locations.remove(item)
-    def __str__(self):
-        x = []
-        for Location in self.locations:
-            x.append(str(Location))
-        return str(x)
+@app.route('/list_of_locations', methods=['GET'])
+def get_information():
+    return list_of_locations
 
-gym = Location("gym")
-lib = Location("lib")
-lab = Location("lab")
+@app.route('/list_of_locations', methods=['POST'])
+def set_location():
+    data = request.get_json()
+    print(data)
+    loc = location(locationName=data['locationName'])
+    db.session.add(loc)
+    db.session.commit()
+    return 'location created', 201
 
-list_of_locations = Locations([gym, lib, lab])
 
-#testing below
-print(list_of_locations)
-list_of_locations.delete(gym)
-print(list_of_locations)
 
 class UserData(Resource):
     def get(self):
         return friendDataStructure
 
-api.add_resource(UserData, "/UserData")
-api.add_resource(Location, "/CacheObjectLocations")
 
+api.add_resource(UserData, "/UserData")
 
 @app.route("/")
 def UniversityGeocache():
@@ -63,3 +53,4 @@ def UniversityGeocache():
 # change for production env
 if __name__ == '__main__':
     app.run(debug=True)
+
