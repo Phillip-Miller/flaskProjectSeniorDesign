7e63fee1e59cec06ecabec6394c5ba4ef8325b46
Ewowodadon <83219152+ewhondens@users.noreply.github.com>
2023-02-28 15:17:14 -0800

    changed database name

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app  # flask instance!
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'user.db'}"  # change later -> database.db or smthn
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'geo.db'}"  # change later -> database.db or smthn
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
