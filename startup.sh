931e99b0426620663976e9ed46e445d0146ac27a
Phillip <phillipmiller@sandiego.edu>
2023-04-11 11:11:23 -0700

    env database  variables dont matter->set to null, enable gunicorn

--- startup.sh
+++ startup.sh
@@ -1,2 +1,5 @@
 export FLASK_CONFIG=config.ProdConfig
 flask --app geocache db upgrade
+gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
+    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
+     --chdir=/home/site/wwwroot app:app
