000054898ecd021215b1422049c53d52d671a5fc
Phillip <phillipmiller@sandiego.edu>
2023-04-11 11:24:49 -0700

    paste in bicep for azure following syntax in example

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 export FLASK_CONFIG=config.ProdConfig
+export FLASK_APP="geocache.__init__:create_app"
 flask --app geocache db upgrade
 gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 \
